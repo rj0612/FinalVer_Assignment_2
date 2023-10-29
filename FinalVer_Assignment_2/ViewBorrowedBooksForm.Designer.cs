bd7408c8d9656adc80a2fd79fe57f58f4735d931
Rei Jiang Wong <Rei Jiang Wong@DESKTOP-UUIC9SV>
2023-10-29 21:52:49 +1100

    Add project files.

---
+++ FinalVer_Assignment_2/ViewBorrowedBooksForm.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace FinalVer_Assignment_2
+{
+    partial class ViewBorrowedBooksForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnBack = new Button();
+            dataGridView1 = new DataGridView();
+            panel2 = new Panel();
+            label1 = new Label();
+            label3 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // btnBack
+            // 
+            btnBack.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            btnBack.Location = new Point(21, 141);
+            btnBack.Name = "btnBack";
+            btnBack.Size = new Size(103, 32);
+            btnBack.TabIndex = 63;
+            btnBack.Text = "Back";
+            btnBack.UseVisualStyleBackColor = true;
+            btnBack.Click += btnBack_Click_1;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(152, 179);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(515, 218);
+            dataGridView1.TabIndex = 59;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(label1);
+            panel2.Controls.Add(label3);
+            panel2.Location = new Point(130, 27);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(555, 113);
+            panel2.TabIndex = 58;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(44, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(448, 45);
+            label1.TabIndex = 0;
+            label1.Text = "Library Management System";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(167, 68);
+            label3.Name = "label3";
+            label3.Size = new Size(202, 32);
+            label3.TabIndex = 7;
+            label3.Text = "Borrowed Books";
+            // 
+            // ViewBorrowedBooksForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnBack);
+            Controls.Add(dataGridView1);
+            Controls.Add(panel2);
+            Name = "ViewBorrowedBooksForm";
+            Text = "ViewBorrowedBooksForm";
+            Load += ViewBorrowedBooksForm_Load_1;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnBack;
+        private DataGridView dataGridView1;
+        private Panel panel2;
+        private Label label1;
+        private Label label3;
+    }
+}
